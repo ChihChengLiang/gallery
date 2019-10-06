a95051bebc1e8f0af65d61304f9d2de9344b2c70
Chih Cheng Liang <chihchengliang@gmail.com>
2019-10-06 22:10:01 +0800

    xkcdfy

--- crypto/toy_curve/curve_on_prime_field.py
+++ crypto/toy_curve/curve_on_prime_field.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from py_ecc.fields.field_elements import FQ
 import mpl_toolkits.axisartist as AA
 
-plt.style.use('grayscale')
 
 q = 97
 
@@ -31,22 +30,23 @@ def gen_points():
 points = list(gen_points())
 print(points)
 xs, y_tops, y_bottoms = list(zip(*points))
-# print(xs, y_tops, y_bottoms)
 
-fig = plt.figure(1, figsize=(4, 4), dpi=100)
-ax = AA.Subplot(fig, 111)
-fig.add_subplot(ax)
-ticks = [16*i for i in range(7)]
-ax.set_xticks(ticks)
-ax.set_yticks(ticks)
-ax.set_xlim(0, 100)
-ax.set_ylim(0, 100)
+with plt.xkcd():
+    fig = plt.figure(1, figsize=(4, 4), dpi=100)
+    ax = AA.Subplot(fig, 111)
+    fig.add_subplot(ax)
+    ticks = [16*i for i in range(7)]
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
 
-ax.axis["right"].set_visible(False)
-ax.axis["top"].set_visible(False)
-for direction in ["left", "bottom"]:
-    ax.axis[direction].set_axisline_style("-|>")
+    ax.axis["right"].set_visible(False)
+    ax.axis["top"].set_visible(False)
+    for direction in ["left", "bottom"]:
+        ax.axis[direction].set_axisline_style("-|>")
 
-ax.scatter(xs, y_tops, marker='.', c='black')
-ax.scatter(xs, y_bottoms, marker='.', c='black')
+    ax.scatter(xs, y_tops, marker='.', c='black')
+    ax.scatter(xs, y_bottoms, marker='.', c='black')
+    ax.set_title(r'$Y^2 = X^3 + 4$ (mod 97)')
 plt.savefig('toy_curve_on_prime_field.png')
